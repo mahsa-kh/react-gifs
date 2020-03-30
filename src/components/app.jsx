8fbc5fb2a441701086006ae96b4cfedc7a1ec131
Mahsa Khamisi <mahsa.khamisi@me.com>
2020-03-30 16:19:24 +0200

    Added API

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import giphy from 'giphy-api';
 import Search from './search.jsx';
 import Gif from './gif.jsx';
 import GifList from './gif_list.jsx';
@@ -8,15 +9,34 @@ class App extends Component {
   constructor (props) {
     super(props);
 
+    this.searchGif("Rick and morty");
+
     this.state = {
       gifs_array: [
-        { id: "tJqyalvo9ahykfykAj" },
-        { id: "WVudyGEaizNeg" },
-        { id: "uZrLs0rcNc9sA" },
-        { id: "ayQ99hp01HFN6" }],
+        // { id: "tJqyalvo9ahykfykAj" },
+        // { id: "WVudyGEaizNeg" },
+        // { id: "uZrLs0rcNc9sA" },
+        // { id: "ayQ99hp01HFN6" }
+        ],
       selectedGifId: "tJqyalvo9ahykfykAj"
     };
   }
+//because I want to change the state of the app, meaning to add gifs ID, based on the search of the textfield
+
+searchGif = (query) => {
+  giphy("7mvC06wg1qzoqol884eCuOuXx7Kbdk8x").search({
+    q: query,
+    rating: 'g' },
+     (error, result) => {
+      // console.log(result); Now I now what is in the api
+      this.setState({
+         gifs_array: result.data //retruns an array with gif id
+      });
+
+  });
+}
+
+
   render () {
 //creating array of gifs to pass to the list
 //gifs
